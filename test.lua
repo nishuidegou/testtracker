e38f9c77640263180def70cefb5184eda2ba9423
nishuidegou <nishuidegou@sina.com>
2016-07-25 01:08:26 +0800

    test diplay frame

--- test.lua
+++ test.lua
@@ -1,4 +1,37 @@
+require 'torch'
 require 'camera'
 require 'qt'
+require 'qtwidget'
+require 'qtuiloader'
+require 'xlua'
+xrequire('nnx',true)
+xrequire('camera',true)
 
-camera.testme()
+require 'torch'
+require 'qt'
+require 'xlua'
+require 'qtwidget'
+require 'qtuiloader'
+xrequire('nnx',true)
+xrequire('camera',true)
+
+
+widget = qtuiloader.load('g.ui')
+window = qt.QtLuaPainter(widget.frame)
+cam = image.Camera{}
+
+function display()
+	frame = cam:forward()
+	image.display{image = frame,win = window,zoom = 1}
+end
+
+timer = qt.QTimer()
+
+
+widget:show()
+
+while true do
+	display()
+end
+
+
